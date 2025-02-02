d94ce2ff31a8eb3bb12b7cdc6f565cd03049a22d
Hiyaa Malik <142331596+hiyaamalik@users.noreply.github.com>
2025-02-02 20:14:42 +0530

    bitwise operations

---
+++ BitwiseOperations.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class BitwiseOperations {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+
+        System.out.println("a & b = " + (a & b));
+        System.out.println("a | b = " + (a | b));
+        System.out.println("a ^ b = " + (a ^ b));
+        System.out.println("~a = " + (~a));
+        System.out.println("a << 1 = " + (a << 1));
+        System.out.println("a >> 1 = " + (a >> 1));
+        System.out.println("a >>> 1 = " + (a >>> 1));
+    }
+}
+
