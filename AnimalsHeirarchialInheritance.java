89a7cce04b9a306e3ee9aa7f70bf7603138cc6d9
Hiyaa Malik <142331596+hiyaamalik@users.noreply.github.com>
2025-02-04 22:53:33 +0530

    Animals Hierarchical Inheritance

---
+++ AnimalsHeirarchialInheritance.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Animals {
+    public void voice() {
+        System.out.println("Animal's Sound");
+    }
+}
+
+class Dog extends Animals {
+    public void voice() {
+        System.out.println("Dog barks");
+    }
+}
+
+class Cat extends Animals {
+    public void voice() {
+        System.out.println("Cat Meows");
+    }
+}
+
+class Lion extends Animals {
+    public void voice() {
+        System.out.println("Lion Roars");
+    }
+}
+
+public class AnimalsHeirarchialInheritance {
+    public static void main(String[] args) {
+        Dog pluto = new Dog();
+        pluto.voice();  
+        System.out.println("Pluto is a dog.");
+    }
+}
