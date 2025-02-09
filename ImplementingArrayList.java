99a9d85f03aa3cb3a9e06a73e731da0a13284d9c
Hiyaa Malik <142331596+hiyaamalik@users.noreply.github.com>
2025-02-09 10:36:10 +0530

    ArrayList

---
+++ ImplementingArrayList.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class ImplementingArrayList{
+  public static void main(String[] args){
+    ArrayList <Integer> numbers = new ArrayList<>();
+    numbers.add(10);
+    numbers.add(20);
+    numbers.add(30);
+    System.out.println(numbers);
+  }
+}
