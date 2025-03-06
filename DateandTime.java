4311335b581090f1b1839cb51fbd6cbcf9e3d4c0
Hiyaa Malik <142331596+hiyaamalik@users.noreply.github.com>
2025-03-06 12:08:29 +0530

    Date and Calendar Package

---
+++ DateandTime.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.util.Date;
+import java.util.Calendar;
+
+public class DateandTime{
+  public static void main (String[] args){
+    Date currentdate = new Date();
+    
+    System.out.println(currentdate);
+    
+    Calendar calendar = Calendar.getInstance();
+    
+    System.out.println(calendar);
+  }
+}
+  
