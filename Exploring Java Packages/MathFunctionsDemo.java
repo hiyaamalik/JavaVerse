4318e515c47d576843dd8efb979193c05f817c68
Hiyaa Malik <142331596+hiyaamalik@users.noreply.github.com>
2025-03-10 12:36:45 +0530

    Math.random(), Math.abs(), and Math.pow()

---
+++ Exploring Java Packages/MathFunctionsDemo.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+
+public class MathFunctionsDemo {
+    public static void main(String[] args) {
+        double randomNumber = Math.random();
+        System.out.println("Random Number: " + randomNumber);
+        
+        int absNum = Math.abs(-7888);
+        System.out.println("Absolute Number: " + absNum);
+        
+        double baseExpo = Math.pow(2,8);
+        System.out.println("2 raise to power 8: " + baseExpo);
+    }
+}
