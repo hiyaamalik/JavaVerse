5f6e087bc5d044cc5b00e7342eace24a2ced16b8
Hiyaa Malik <142331596+hiyaamalik@users.noreply.github.com>
2025-04-02 13:28:51 +0530

    Subtraction

---
+++ customCalc Package/Calc/Sub.java
@@ -0,0 +1,15 @@
+package Calc;
+
+public class Sub{
+    void sub(int a){
+        System.out.println(a);
+    }
+
+    void sub(int a, int b){
+        System.out.println(a - b);
+    }
+
+    void sub(int a, int b, int c){
+        System.out.println(a - b - c);
+    }
+}
