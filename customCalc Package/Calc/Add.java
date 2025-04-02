fc726d9362f68e7248e8c6ae8dfff12fa84c517b
Hiyaa Malik <142331596+hiyaamalik@users.noreply.github.com>
2025-04-02 13:29:34 +0530

    Addition

---
+++ customCalc Package/Calc/Add.java
@@ -0,0 +1,15 @@
+package Calc;
+
+public class Add{
+    public void add(int a){
+        System.out.println(a);
+    }
+
+    public void add(int a, int b){
+        System.out.println(a + b);
+    }
+
+    public void add(int a, int b, int c){
+        System.out.println(a + b + c);
+    }
+}
